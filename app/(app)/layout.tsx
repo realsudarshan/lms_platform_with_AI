0152b112a5cfef470d5e64a4f6c5c22d7b26449c
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2025-12-15 20:54:13 +0545

    fix(structure): seprate pages for student and admin dashboard

---
+++ app/(app)/layout.tsx
@@ -0,0 +1,11 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
+function AppLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <ClerkProvider>
+      <div>{children}</div>
+    </ClerkProvider>
+  );
+}
+
+export default AppLayout;
