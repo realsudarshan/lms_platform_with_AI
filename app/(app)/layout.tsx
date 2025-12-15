f3063002351eb1c49d5125d8eb8af5a9281052dc
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2025-12-15 21:11:59 +0545

    feat(layout): header comp added

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,9 +1,12 @@
+import { Header } from "@/components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
 
 function AppLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <ClerkProvider>
+        <Header/>
       <div>{children}</div>
+      
     </ClerkProvider>
   );
 }
