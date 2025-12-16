2a80c01d4e783e6cc83afc9e8e32206c65f564ee
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2025-12-16 13:28:07 +0545

    fix(sanity): all type imported in indexschema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,8 +1,10 @@
 import type { SchemaTypeDefinition } from "sanity";
 import { categoryType } from "./categoryType";
-
+import { courseType } from "./courseType";
+import { lessonType } from "./lessonType";
+import { moduleType } from "./moduleType";
 
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [categoryType],
+  types: [courseType, moduleType, lessonType, categoryType],
 };
