07fcd6d299656d9b9ac45207725c1aac884d2495
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2025-12-21 22:14:58 +0545

    Fix studio client rendering

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,19 +1,24 @@
-/**
- * This route is responsible for the built-in authoring environment using Sanity Studio.
- * All routes under your studio path is handled by this file using Next.js' catch-all routes:
- * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
- *
- * You can learn more about the next-sanity package here:
- * https://github.com/sanity-io/next-sanity
- */
+'use client'
 
+import { useAuth } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 import { NextStudio } from 'next-sanity/studio'
-import config from '../../../sanity.config'
-
-export const dynamic = 'force-static'
-
-export { metadata, viewport } from 'next-sanity/studio'
+import config from '@/sanity.config'
 
 export default function StudioPage() {
+  const { userId, isLoaded } = useAuth()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (isLoaded && !userId) {
+      router.push('/sign-in')
+    }
+  }, [userId, isLoaded, router])
+
+  if (!isLoaded || !userId) {
+    return <div>Loading...</div>
+  }
+
   return <NextStudio config={config} />
 }
